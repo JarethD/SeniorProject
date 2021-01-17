6547b0f764dd7923b4469b4639b4e553550c8711
Jareth <jarethdodson9876@gmail.com>
2021-01-16 17:15:53 -0800

    Added sort by priority and status to Truckdriver class

--- WindowsApp/DesktopApp/TruckDriver.cs
+++ WindowsApp/DesktopApp/TruckDriver.cs
@@ -8,18 +8,77 @@ namespace DesktopApp
 {
     class TruckDriver
     {
+        private string m_trUsername { get; set; }
+        private string m_trHashPass { get; set; }
         private string m_trName { get; set; }
         private int m_trPhoneNum { get; set; }
         private string m_trAddress { get; set; }
         private string m_trCompany { get; set; }
         private int m_trID { get; set; }
-        public TruckDriver(string name, int num, string address, string company)
+        private List<Order> m_trOrders { get; set; }
+        public TruckDriver(string username, string hashpass, string name, int num, string address, string company)
         {
+            m_trUsername = username;
+            m_trHashPass = hashpass;
             m_trName = name;
             m_trPhoneNum = num;
             m_trAddress = address;
             m_trCompany = company;
             //Grab greatest ID number from database and increment by and set m_trID to new ID 
+        }
+        public void AddOrder(Order newOrder)
+        {
+            m_trOrders.Append(newOrder);
+        }
+        public void DeleteOrder(Order oldOrder)
+        {
+            m_trOrders.Remove(oldOrder);
+        }
+
+        public void SortByPriority()
+        {
+            List<Order> tempList = new List<Order>();
+            for (int i = 0; i < 3; i++) // 3 is amount of items in priority enum
+            {
+                foreach (Order order in m_trOrders)
+                {
+                    if (order.m_oPriority == (priority)i)
+                    {
+                        tempList.Append(order);
+                    }
+                }
+            }
+            if (m_trOrders.Count != 0)
+            {
+                m_trOrders.Clear();
+                foreach (Order order in tempList)
+                {
+                    m_trOrders.Append(order);
+                }
+            }
+        }
+        public void SortByStatus()
+        {
+            List<Order> tempList = new List<Order>();
+
+            for (int i = 0; i < 4; ++i)
+            {
+                foreach (Order order in m_trOrders)
+                {
+                    if (order.m_oStatus == (status)i)
+                    {
+                        tempList.Append(order);
+                    }
+                }
+            }
+            if (m_trOrders.Count != 0)
+            {
+                m_trOrders.Clear();
+                foreach (Order order in tempList)
+                {
+                    m_trOrders.Append(order);
+                }
+            }
         }
     }
 }
