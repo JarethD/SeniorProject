15ac5c8f590bdf8a0b912c81c1e0e67917013315
Jareth <jarethdodson9876@gmail.com>
2021-01-31 23:07:32 -0800

    This commit is actually adding the Add Company and Add Employee to the respective database. Last commit was actually for executing a query for a single company or employee. Still need to add database code for orders.

--- WindowsApp/Database_Helpers/UserData.cs
+++ WindowsApp/Database_Helpers/UserData.cs
@@ -15,10 +15,40 @@ namespace Database_Helpers
 
         public int AddTruckDriver(TruckDriver newDriver)
         {
-            string query = String.Format("INSERT INTO EMPLOYEES " +
-                "VALUES('{0}', '{1}','{2}','{3}','{4}','{5}', '{6}')", 
+            string query = String.Format("INSERT INTO EMPLOYEE " +
+                "VALUES('{0}', '{1}','{2}','{3}','{4}','{5}', '{6}')",
                 newDriver.username, newDriver.HashPass, newDriver.ID,
                 newDriver.Phonenum, newDriver.Company, newDriver.Address);
+
+            return _database.ExecuteQuery_NoReturnType(query);
+        }
+
+        public int AddLumberAssociate(LumberAssociate newLA)
+        {
+            string query = String.Format("INSERT INTO EMPLOYEE " +
+            "VALUES('{0}', '{1}','{2}','{3}','{4}','{5}', '{6}')",
+            newLA.username, newLA.HashPass, newLA.ID,
+            newLA.Phonenum, newLA.Company, newLA.Company);
+
+            return _database.ExecuteQuery_NoReturnType(query);
+        }
+
+        public int AddLumberCompany(LumberAssociate newCompany)
+        {
+            string query = String.Format("INSERT INTO COMPANY " +
+            "VALUES('{0}', '{1}','{2}','{3}','{4}','{5}', '{6}')",
+            newCompany.username, newCompany.HashPass, newCompany.ID,
+            newCompany.Phonenum, newCompany.Company, newCompany.Company);
+
+            return _database.ExecuteQuery_NoReturnType(query);
+        }
+
+        public int AddHardwarStore(LumberAssociate newStore)
+        {
+            string query = String.Format("INSERT INTO COMPANY " +
+            "VALUES('{0}', '{1}','{2}','{3}','{4}','{5}', '{6}')",
+            newStore.username, newStore.HashPass, newStore.ID,
+            newStore.Phonenum, newStore.Company, newStore.Company);
 
             return _database.ExecuteQuery_NoReturnType(query);
         }
